bdf8484237c33bfa9e72640a0edb29266a17c9ca
MnyaMnya <i60889958@gmail.com>
2022-03-25 12:34:04 +0700

    a

--- js/home.js
+++ js/home.js
@@ -1,9 +1,8 @@
-const targetDiv = document.getElementById("div3");
-const btn = document.getElementById("toggle");
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
-  } else {
-    targetDiv.style.display = "block";
-  }
+function a() {
+    const targetDiv = document.getElementById("div3");
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+    } else {
+        targetDiv.style.display = "block";
+    }
 };
