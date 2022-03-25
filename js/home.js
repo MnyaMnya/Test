1077f225c169b4a1fc30dbc090eb54e0a856c325
MnyaMnya <i60889958@gmail.com>
2022-03-25 10:50:39 +0700

    Added Home

---
+++ js/home.js
@@ -0,0 +1,9 @@
+const targetDiv = document.getElementById("div3");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
