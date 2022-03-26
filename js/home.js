7dba451a62158a10e3491ea92d85dad6b587e1d7
MnyaMnya <i60889958@gmail.com>
2022-03-26 15:20:44 +0700

    add img

--- js/home.js
+++ js/home.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $("#toggle").click(function() {
         $("#div3").slideToggle("slow");
     });
+    $("#ha").click(function() {
+        $("#div3a").slideToggle("slow");
+    });
 });
