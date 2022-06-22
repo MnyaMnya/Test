2b28763f261e3d42b48d2b2cf631c18b5f77404d
MnyaMnya <i60889958@gmail.com>
2022-06-22 20:44:17 +0700

    new sort algoryhtm

--- c ++ code/sort.cpp
+++ c ++ code/sort.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 int main(){
     int ref;
-    int a[5];
+    int n;
     line1:
-    for (int i=0;i<5;i++){
+    cout << "Ketik Jumlah Bilangan yang mau di urutkan: ";
+    cin >> n;
+    int a[n];
+    for (int i=0;i<n;i++){
         cout << "Ketik Nomor Urut Ke " << i+1<<" : ";
         cin >> a[i];
     }
-    for (int i=0;i<5;i++){
-        for (int j=i+1;j<5;j++){
+    for (int i=0;i<n;i++){
+        for (int j=i+1;j<n;j++){
             if(a[j]<a[i]){
                 ref = a[i];
                 a[i] = a[j];
@@ -18,10 +21,11 @@ int main(){
         }
     }
     cout <<"Urutanya adalah: ";
-    cout << "{ ";
-    for (int i=0;i<5;i++){
+    cout << "{";
+    for (int i=0;i<n-1;i++){
         cout << a[i]<<", ";
     }
+    cout << a[n-1];
     cout << "}" << endl;
     goto line1;
 }
