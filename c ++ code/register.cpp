d41090ae7fa14ac770d5050006aa57da394d7b95
MnyaMnya <i60889958@gmail.com>
2022-06-24 18:51:53 +0700

    new registration programm

--- c ++ code/register.cpp
+++ c ++ code/register.cpp
@@ -25,8 +25,24 @@ void test(){
 	 sleep (1);
 	 system("cls");
 }
+void quit(){
+	 string load;
+	 load = "Quitting";
+	 cout << "\r" << load << flush;
+	 sleep (1);
+	 load = "Quitting.";
+	 cout << "\r" << load << flush;
+	 sleep (1);
+	 load = "Quitting..";
+	 cout << "\r" << load << flush;
+	 sleep (1);
+	 load = "Quitting...";
+	 cout << "\r" << load << flush;
+	 sleep (1);
+	 terminate();
+}
 void six(){
-    for(int i=1;i<600;i++){
+    /*for(int i=1;i<600;i++){
 cout << "666666666666666666666666666666";
     }
 sleep (3);
@@ -51,64 +67,135 @@ cout << "Stop!!!!!";
 sleep (3000);
 system("cls");
 terminate();
+*/
+system("cls");
+cout << "Password anda salah";
+sleep(1);
+quit();
 }
 
 int main(){
 	string user,pass,mypass,reg;
+	string cpass;
+	bool x;
 	fstream txtsize("size.txt");
 	string ssize,line;
 	getline (txtsize, ssize);
 	int size = stoi(ssize);
-	fstream txtuser("user.txt");
+	fstream txtuser;
+	txtuser.open("user.txt");
 	string username[size+1];
-		for(int i=0;i<size;i++){
-			getline (txtuser,line);
-			username[i]=line;
-		}
+	for(int i=0;i<size;i++)
+	{
+		getline (txtuser,line);
+		username[i]=line;
+	}
+	line1:
 	cout << "Apakah anda ingin register atau check akun atau melihat list[R/C/L]"<<endl;
 	cout << "Ketik Input anda: ";
 	cin >> reg;
 	system("cls");
-	if (reg =="C" || reg=="c"){
+	if (reg =="C" || reg=="c")
+	{
 		cout << "Ketik Username anda: ";
 		cin >> user;
 		ifstream txt(user+".txt");
 		cout << "Ketik Password anda: ";
 		cin >> pass;
 		getline (txt, mypass);
-		if (pass != mypass){
+		if (pass != mypass)
+		{
 		system("cls");
         system("color 4e");
 		six();
+		}
+		else {}
 	}
-	}
-	if (reg =="R" || reg=="r"){
+	else if (reg =="R" || reg=="r")
+	{
 		cout << "Ketik Username anda: ";
 		cin >> user;
-		ofstream txt(user+".txt");
-		cout << "Ketik Password anda: ";
-		cin >> pass;
-		txt << pass;
-		for(int i=0;i<size;i++){
-			if (user == username[i]){
-				cout << "User ini sudah register"<<endl;
+		for(int i=0;i<size;i++)
+		{
+		if (user == username[i])
+		{
+			x=true;
+		}
+		else{}
+		}
+		if (x == true)
+		{
+			line2:
+			cout << "User ini sudah register"<<endl<<"Apakah anda mau mengganti Password? [Y/N]"
+			<<endl<<"Ketik input anda:";
+			cin >> cpass;
+			if (cpass == "n" || cpass == "N")
+			{
+				cout << "Quitting.."<<endl;
 				terminate();
 			}
+			else if (cpass == "y" || cpass == "Y")
+			{
+
+				fstream txt(user+".txt");
+				cout << "Ketik Password anda: ";
+				cin >> pass;
+				getline (txt, mypass);
+				if (pass != mypass)
+				{
+					system("cls");
+       				system("color 4e");
+					six();
+				}
+				else
+				{
+					txt.clear();
+					txt.seekg(0);
+					cout << "Ketik Password Baru anda: ";
+					cin >> pass;
+					txt <<pass;
+				}
+			}
+			else
+			{
+				cout << "Input ini tidak bisa dipakai"<<endl;
+				sleep(1);
+				goto line2;
+			}
+			
 		}
-		size += 1;
-		ofstream txtsizes("size.txt");
-		txtsizes<<size;
-		username[size-1]= user;
-		for(int i=0;i<size;i++){
-			txtuser<<username[i];
+		else 
+		{
+			ofstream txt(user+".txt");
+			cout << "Ketik Password anda: ";
+			cin >> pass;
+			txt << pass;
+			size += 1;
+			ofstream txtsizes("size.txt");
+			txtsizes<<size;
+			txtsizes.close();
+			txtuser.close();
+			username[size-1]= user;
+			txtuser.open("user.txt");
+			for(int i=0;i<size;i++)
+			{
+				txtuser<<username[i]<<endl;
+			}
 		}
 	}
-	if (reg =="L" || reg=="l"){
+	else if (reg =="L" ||reg=="l")
+	{
 		cout << "list user yang diregistrasi:"<<"["<<size<<"]" <<endl;
-		for(int i=0;i<size;i++){
+		for(int i=0;i<size;i++)
+		{
 			cout << username[i]<<endl;
 		}
 		cin >> reg;
+	}
+	else {
+		cout << "Input ini tidak bisa dipakai"<<endl;
+		sleep(1);
+		goto line1;
 	}
 	system("cls");
 	cout << "Login Sebagai:"<<user<<endl;
