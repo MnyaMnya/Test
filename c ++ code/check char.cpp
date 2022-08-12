6adb5ef52204b93a41e5e24162373207c96e3ed7
MnyaMnya <i60889958@gmail.com>
2022-08-12 09:43:16 +0700

    enable lowarcase and upper case check in check char.cpp

--- c ++ code/check char.cpp
+++ c ++ code/check char.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <string.h>
+#include <cctype>
 using namespace std;
 int main(){
 int counter=0;
-char txt[5000],checker;
+char txt[5000],checker,ucheck,lcheck;
 cout << "Ketik Textnya: ";
 cin >> txt;
 cout << "Ketik Huruf yang mau dicari: ";
 cin >> checker;
+ucheck = toupper(checker);
+lcheck = tolower(checker);
 int len = strlen(txt);
 for (int i=0;i<len;i++){
-    if(txt[i]==checker){
+    if(txt[i]==ucheck ||txt[i]== lcheck ){
         counter++;
     }
 }
-cout << "Jumlah Huruf ["<<checker<<"] Yang ada di textnya ada ("<<counter<<")";
+cout << "Jumlah Huruf ["<<ucheck<<"] Yang ada di textnya ada ("<<counter<<")";
 cin >> txt;
 }
