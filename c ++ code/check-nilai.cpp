3d45ff127263c43a5c19e2dffb0f5f57121dda52
MnyaMnya <i60889958@gmail.com>
2022-08-12 11:17:57 +0700

    ero;si[awjfpoahgw

--- c ++ code/check-nilai.cpp
+++ c ++ code/check-nilai.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
 
 int main() {
     int size,nilai[size];
-    int ref,counter=0;
-    //int nilai [8] : (81,78,90,97,85,97,97,88);
+    int ref,counter=1;
+    //int nilai [8] : (81,90,78,97,85,97,97,88);
     cin >> size;
     for (int i=0;i<size;i++){
         cin >> nilai[i];
     }
+    for (int i=0;i<size;i++){
+        for (int j=i+1;j<size;j++){
+            if(nilai[j]<nilai[i]){
+                ref = nilai[i];
+                nilai[i] = nilai[j];
+                nilai[j] = ref;
+            }
+        }
+    }
     for (int i=0;i<size-1;i++){
-        if(nilai[i]<nilai[i+1]){
-            ref = nilai[i];
-            nilai[i]=nilai[i+1];
-            nilai[i+1]=ref;
-        }
-    }
-    cout << "I[0]nya adalah: "<< nilai[0]<<endl;
-    for (int i=1;i<size;i++){
-        if(nilai[0]==nilai[i]){
+        if(nilai[size-1]==nilai[i]){
             counter++;
-            cout << "Yang sama dengan nilai terbesar: "<<nilai[i]<<endl;
+            cout << "counter added"<<endl;
         }
     }
-    cout << counter;
- 
+cout << counter;
+ cin >> counter;
   return 0;
 }
