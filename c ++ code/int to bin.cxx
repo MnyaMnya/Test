5bc8dfa25c85c719db956deb2b6bc228970f1313
MnyaMnya <81231166+MnyaMnya@users.noreply.github.com>
2023-09-26 13:46:58 +0700

    fix int to bin

--- c ++ code/int to bin.cxx
+++ c ++ code/int to bin.cxx
@@ -7,18 +7,18 @@ int main(){
 	string bin="";
 	cout << "Ketik nilai yang mau di biner kan:";
 	cin >> a;
-	/*cout << "Biner basis brp: ";
-	cin >> b;
-	*/
-b=2;
+	cout << "Biner basis brp: ";
+	cin>>b;
 	while (a >= 1){
 			ref = a;
 			a= a/b;
-			if (ref % b != 0){
-					bin =  "1"+ bin;
+			back:
+			if (ref % b <= b){
+					bin =  to_string(ref%b)+ bin;
 				}
-				else{
-					bin = "0"+ bin;
+				if(ref >= b && ref%b >= b){
+					ref= ref/10;
+					goto back;
 				}
 	}
 		cout << "Jawabanya adalah: "<<bin<<endl;
